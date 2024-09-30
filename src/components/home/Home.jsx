0ed0961ed74295e5569b802cc88ea9a4710f950b
Prathmesh Pardeshi <prathmeshpardeshi@Prathmeshs-MacBook-Air.local>
2024-09-30 17:29:04 -0400

    Added Google Analytics tracking

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,10 +3,13 @@ import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
 import Scroll from "./Scroll";
+import ReactGA from 'react-ga4'
 
 
 const Home = () => {
-  
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search, title: "Home Page" });
+  }, []);
   return (
     <section className="home section" id="home">
             <div className="home__container container grid">
